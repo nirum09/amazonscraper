6fa61a8d2dc8de7d27e1db3f22898c09dc120bb1
nirum09 <nirupama.m09@gmail.com>
2020-05-25 17:39:25 +0530

    Adding scraper

--- amazonscraper/amazonproduct/models.py
+++ amazonscraper/amazonproduct/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class searchitem(models.Model):
+    item_name= models.TextField()
+    item_price= models.DecimalField()
+    item_rating = models.DecimalField()
+    item_numrating = models.PositiveIntegerField()
---
+++ scraper/scraper/items.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy_djangoitem import DjangoItem
+from amazonscraper.amazonproduct import models
+
+class ScraperItem(DjangoItem):
+    # define the fields for your item here like:
+    django_model = models.searchitem
